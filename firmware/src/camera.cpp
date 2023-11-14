0472d631eadc5cf23b0fb897a52043e7549c8b31
Mauricio D. Demoro <mddemoro@gmail.com>
2023-11-14 10:50:10 -0300

    Create camera module

---
+++ firmware/src/camera.cpp
@@ -0,0 +1,60 @@
+#include "camera.h"
+
+
+void CameraInit() {
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.frame_size = FRAMESIZE_UXGA;
+    config.pixel_format = PIXFORMAT_JPEG; // for streaming
+    //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+
+    pinMode(PWDN_GPIO_NUM, OUTPUT);
+    digitalWrite(PWDN_GPIO_NUM, LOW);
+
+    //pinMode(LED_GPIO_NUM, OUTPUT);
+    //digitalWrite(LED_GPIO_NUM, HIGH);
+
+    esp_camera_init(&config);
+}
+
+uint8_t CameraCapture(uint8_t * jpg_buf, size_t * jpg_buf_len) {
+    camera_fb_t * fb;
+
+    Serial.println("Tomando foto");
+
+    fb = esp_camera_fb_get();
+
+    if(!fb) {
+        Serial.println("Error al tomar foto");
+        return 1;
+    }
+
+    jpg_buf = fb->buf;
+    *jpg_buf_len = fb->len;
+
+    esp_camera_fb_return(fb);
+
+    return 0;
+}
