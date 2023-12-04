65d7ec6285cb86a33163e6b5669a4ab056e79ee6
Mauricio D. Demoro <mddemoro@gmail.com>
2023-12-04 15:58:39 -0300

    Changed pins for servos and base motor, and camera no longer uses flash

--- firmware/include/base_motor.h
+++ firmware/include/base_motor.h
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 
-#define IN1 16 // Pin IN1 del driver conectado al GPIO16 (D0)
-#define IN2  0 // Pin IN2 del driver conectado al GPIO16 (D1)
-#define IN3  3 // Pin IN3 del driver conectado al GPIO16 (D2)
-#define IN4  1 // Pin IN4 del driver conectado al GPIO16 (D3)
-#define SW   2 // Limit switch para calcular posicion inicial
+#define IN1 12 // Pin IN1 del driver conectado al GPIO16 (D0)
+#define IN2 13 // Pin IN2 del driver conectado al GPIO16 (D1)
+#define IN3 15 // Pin IN3 del driver conectado al GPIO16 (D2)
+#define IN4 14 // Pin IN4 del driver conectado al GPIO16 (D3)
+#define SW   3 // Limit switch para calcular posicion inicial
 
 // Límites del ángulo de la base expresados en mediciones del potenciómetro
 #define SPR  2048 // Steps per revolution del motor
